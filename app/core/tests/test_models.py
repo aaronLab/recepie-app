e622423b1aafc8ce1bd7ee0763b5064e04ebe86b
Aaron Lee <aaronlab.net@gmail.com>
2021-03-11 19:16:38 +0900

    email validation

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -27,3 +27,11 @@ class ModelTests(TestCase):
 
         self.assertEquals(user.email, email.lower())
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                email=None,
+                password='Password1234'
+            )
